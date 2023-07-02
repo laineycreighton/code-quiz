4a4bdf090be2a80cf7608b3d34d4b13c246a97b8
Lainey Creighton <dev.lainey@gmail.com>
2023-07-01 19:00:48 -0700

    add start to ask.css & consolidated js files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
 // Create global variables
     var scores = 0;
-    var timeLeft = 120;
-    var questions = ["What color is a black bear?", "You make a turn while hiking and suddenly you are 50 feet from a bear that sees you. What do you do?", "You're camping in bear country. What items do you not put in your bear container?"]; // made of a string
+    var questions = ["What color is a black bear?", "You make a turn while hiking and suddenly you are 50 feet from a bear that sees you. What is the first thing that you do?", "You're camping in bear country. What items do you not put in your bear container?"]; // made of a string
     var answersChoices = [["Black","Brown","White","All of the above"], ["Turn and run!","Scream!","Play dead","Stand your ground and talk to the bear."], ["Granola Bars","Toothpaste","Bear Spray","Cooking Utensils"]]; // made of arrays of strings
-    var correctAnswer = ["Answer 1", "Answer 2", "Answer 3"]; // made of strings
+    var correctAnswer = ["All of the above", "Stand your ground and talk to the bear.", "Bear Spray"]; // made of strings
     var currentQuestion = 0;
 
 // HTML selectors
 //the id of each question
 
-var questionsEl = document.querySelector("#question")
-
+var questionsEl = document.querySelector("#question");
+var startBtn = document.querySelector("#start-quiz");
 
 // Functions
 //ask.html
 function startQuiz() {
+
     // triggered when the button is clicked
     // add eventListener for "click"
     // start the timer when function is called
@@ -25,12 +25,58 @@ function startQuiz() {
 }
 
 //linked to the start button
-startBtn.addEventListener("start-quiz", startQuiz);
+startBtn.addEventListener("click", startQuiz);
 
 var startBtn = document.querySelector("#start-quiz")
 // eventListener to call the startQuiz function with the "start" button
 
 
+//this var calls the "timer" id from the ask.html
+var timerEl = document.querySelector("#timer");
+// this var calls the "bear-attack" image from the ask.html
+var timeEnds = document.querySelector("bear-attack");
+var timeLeft = 60
 
+
+timerEl.addEventListener()
+
+function startTimer() {
+    console.log("Time Begins Now");
+    var timer = setInterval(function () {
+        timeLeft--;
+        timerEl.textContent = timeLeft = " seconds remaining.";
+        if (timeLeft === 0) {
+            clearInterval(timer);
+            sendMessage();
+        }
+    }, 1000);
+}
+
+function sendMessage() {
+    console.log("Time's Up!");
+    timerEl.textContent = " ";
+    var attackImg = document.createElement("img");
+    attackImg.setAttribute("src", "images/bear-attack.png");
+    timeEnds.appendChild(attackImg);
+
+}
+
+function nextQuestion() {
+    // triggered when the user selects an answer
+    // accept the event ("click") parameter to know which answer the user clicked
+    // determine if the answer is right or wrong
+    // if else statements
+    // if wrong, adjust time
+    // if right, add to score
+    // increment the current question by 1
+    // display next question and choices element
+}
+
+function endQuiz() {
+    // triggered when timer ends OR last question is answered
+    // hide questions
+    // display the score and prompt user for initials
+    // stop the timer
+}
 
 
